4723a49dec34cab08e17719d78e29834f4321e94
Mark <mark@lnreader.com>
2016-01-24 02:01:28 +0700

    added force update for image tag

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,5 @@ VERSION="1.0.0"
 
 docker build -t ${IMAGE}:${VERSION} . | tee docker-build.log || exit 1
 ID=$(tail -1 docker-build.log | awk '{print $3;}')
-docker tag $ID ${IMAGE}:latest
+docker tag $ID -f ${IMAGE}:latest
 docker images | grep ${IMAGE}
